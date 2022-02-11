6a7248434dcac06c6690b85ef2d990ac34ccae8f
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-02-11 12:31:42 +0200

    10-Добавление Модального Окна

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from "../UI/Modal";
 import styles from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -10,7 +11,7 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={styles.total}>
         <span>Итого</span>
@@ -20,7 +21,7 @@ const Cart = (props) => {
         <button className={styles["button--alt"]}>Закрыть</button>
         <button className={styles.button}>Заказать</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
