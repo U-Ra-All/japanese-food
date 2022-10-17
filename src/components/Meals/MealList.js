9260a00c1f5860cb90c90bfcdf268ddd8cce7347
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-10-17 11:00:03 +0300

    04-Управление Состоянием Загрузки

--- src/components/Meals/MealList.js
+++ src/components/Meals/MealList.js
@@ -5,9 +5,11 @@ import MealItem from "./MealItem/MealItem";
 
 const MealList = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
       const response = await fetch(
         "https://react-course-http-8220d-default-rtdb.firebaseio.com/meals.json"
       );
@@ -25,10 +27,19 @@ const MealList = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.loading}>
+        <p>Извлечение данных с сервера...</p>
+      </section>
+    );
+  }
 
   const mealList = meals.map((meal) => (
     <MealItem
