4ff7a78b14d7bc89d152d21d614a39cbb11a5973
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-10-18 12:15:10 +0300

    07-Получение Данных из Формы

--- src/components/Cart/SubmitOrder.js
+++ src/components/Cart/SubmitOrder.js
@@ -1,28 +1,39 @@
+import { useRef } from "react";
 import styles from "./SubmitOrder.module.css";
 
 const SubmitOrder = (props) => {
+  const nameInputRef = useRef();
+  const cityInputRef = useRef();
+  const addressInputRef = useRef();
+
   const confirmOrderHandler = (event) => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
   };
 
   return (
-    <form onSubmit={confirmOrderHandler}>
+    <form className={styles.form} onSubmit={confirmOrderHandler}>
       <div className={styles.control}>
         <label htmlFor="name">Введите Имя</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="city">Введите Название Города</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="address">Введите Адрес</label>
-        <input type="text" id="address" />
+        <input type="text" id="address" ref={addressInputRef} />
       </div>
-      <button>Подтвердить Заказ</button>
-      <button type="button" onClick={props.onCancel}>
-        Отменить
-      </button>
+      <div className={styles.actions}>
+        <button className={styles.submit}>Подтвердить Заказ</button>
+        <button type="button" onClick={props.onCancel}>
+          Отменить
+        </button>
+      </div>
     </form>
   );
 };
